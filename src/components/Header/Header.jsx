f07e39c81aa0da950ea35da06d1fd8814129d016
falaprodavi <falaprodavo@gmail.com>
2024-10-11 15:28:51 -0300

    feat(ui): Implement Hero Section on homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
 import logo from "../../assets/images/logo.png";
+import userImage from "../../assets/images/avatar-icon.png";
 import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 
 const navLinks = [
   {
@@ -22,8 +24,31 @@ const navLinks = [
 ];
 
 function Header() {
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const handleStickHeader = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTo > 80
+      ) {
+        headerRef.current.classList.add("stick__header");
+      } else {
+        headerRef.current.classList.remove("stick__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleStickHeader();
+    return () => window.removeEventListener("scroll", handleStickHeader);
+  });
+
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* ================ logo ================ */}
@@ -32,7 +57,7 @@ function Header() {
           </div>
 
           {/* ================ menu ================ */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
               {navLinks.map((link, index) => (
                 <li key={index}>
@@ -49,6 +74,27 @@ function Header() {
                 </li>
               ))}
             </ul>
+          </div>
+          {/* ================ nav rigth ================ */}
+
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-9 h-9 rounded-full cursor-pointer">
+                  <img src={userImage} className="w-full rounded-full" alt="" />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-semibold items-center justify-center rounded-lg">
+                Login
+              </button>
+            </Link>
+
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,75 @@
 import React from "react";
 
+import heroImg01 from "../assets/images/hero-img01.png";
+import heroImg02 from "../assets/images/hero-img02.png";
+import heroImg03 from "../assets/images/hero-img03.png";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* ================ hero section ================ */}
+
+      <section className="hero__section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* ================ hero content ================ */}
+            <div>
+              <div className="lg:w-[570px]">
+                <h1 className="text-3xl mb-4 leading-[46px] text-headingColor font-extrabold md:text-6xl md:leading-[70px]">
+                  We help patients live a healthy, longer life.
+                </h1>
+                <p className="text-para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam, explicabo quidem, soluta repellendus amet officiis
+                  fugit sunt quas quasi consequuntur impedit voluptas
+                  accusantium rerum numquam doloremque provident totam maxime.
+                  Dolore.
+                </p>
+                <button className="btn">Request an Appointment</button>
+              </div>
+              {/* ================ hero counter ================ */}
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col md:flex-row md:items-center gap-5 md:gap-[30px]">
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Years of Experience</p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    15+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Clinic Location</p>
+                </div>
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Patient Satisfaction</p>
+                </div>
+              </div>
+            </div>
+
+            {/* ================ hero content ================ */}
+
+            <div className="flex gap-[30px] justify-end">
+              <div>
+                <img src={heroImg01} alt="" className="w-full" />
+              </div>
+              <div className="mt-[30px]">
+                <img src={heroImg02} alt="" className="w-full mb-[30px]" />
+                <img src={heroImg03} alt="" className="w-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* ================ hero section end ================ */}
+    </>
+  );
 }
 
 export default Home;
